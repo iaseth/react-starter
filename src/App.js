1790ed04044495057893f34f7ce3c6ff2f8c2423
Ankur Seth <iankurseth@gmail.com>
2022-02-11 07:31:18 +0530

    Corrected URL

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App () {
 			<main className="flex min-h-screen bg-red-500">
 				<div className="text-white text-center m-auto">
 					<h2 className="text-4xl py-6">React Starter</h2>
-					<a href="https://iaseth.github.io/react-starter" target="_blank" className="px-4 py-3 text-sm bg-red-600 duration-300 hover:bg-red-700">GitHub</a>
+					<a href="https://github.com/iaseth/react-starter" target="_blank" rel="noreferrer" className="px-4 py-3 text-sm bg-red-600 duration-300 hover:bg-red-700">GitHub</a>
 				</div>
 			</main>
 		</div>
