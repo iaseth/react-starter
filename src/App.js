dabd62ceb1a831c54b37f8f6484b69e2c47e3793
Ankur Seth <iankurseth@gmail.com>
2022-02-11 07:22:14 +0530

    Hallo Welt

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+
+function App () {
+	return (
+		<div className="App">
+			<main className="flex min-h-screen bg-red-500">
+				<div className="text-white text-center m-auto">
+					<h2 className="text-4xl py-6">React Starter</h2>
+					<a href="https://iaseth.github.io/react-starter" target="_blank" className="px-4 py-3 text-sm bg-red-600 duration-300 hover:bg-red-700">GitHub</a>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
